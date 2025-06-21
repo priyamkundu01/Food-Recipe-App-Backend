ba57968dc01bd53523441f130916dd8ac55b6be1
User1 Priyam Kundu <priyamkundu4411@gmail.com>
2025-06-21 20:09:35 +0530

    delete record from favouritesTable in db

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favouritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
 
 const app = express();
 const PORT = ENV.PORT || 5001;
@@ -33,6 +34,21 @@ app.post('/api/favourites', async (req, res) => {
 
   } catch (error) {
     console.log('Error adding favourite', error);
+    res.status(500).json({ error: 'Something went wrong!' });
+  }
+})
+
+app.delete('/api/favourites/:userId/:recipeId', async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db.delete(favouritesTable).where(
+      and(eq(favouritesTable.userId, userId), eq(favouritesTable.recipeId, parseInt(recipeId)))
+    )
+
+    res.status(200).json({ message: 'Favourite removed successfully!' });
+  } catch (error) {
+    console.log('Error removing a favourite', error);
     res.status(500).json({ error: 'Something went wrong!' });
   }
 })
